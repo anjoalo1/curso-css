63fbdd5c1a2be2da79bb83bf1dc10c455d9592ca
anjoalo1@gmail.com <anjojalo1@gmail.com>
2023-08-15 23:39:47 -0500

    agregando-otros-selectores

--- js/scriptjsindex.js
+++ js/scriptjsindex.js
@@ -1,22 +1,36 @@
-
-
 const asideLeft = document.getElementById('aside-left');
 const botonBurger = document.getElementById('burger-icon');
 const containerMain = document.getElementById('container-page-main');
 const pagemain = document.getElementById('container-main');
+const imgclick = document.getElementsByTagName('img');
+const modal = document.getElementById('modal');
+const imagemodal = document.getElementById('image-modal');
+const closemodal = document.getElementById('close-modal');
+
+const div = document.createElement('div');
+
+ containerMain.addEventListener('click', (e)=>{
+    if(e.target.hasAttribute('src')){
+        let mysrce= e.target.getAttribute("src");
+        imagemodal.setAttribute('src', mysrce);
+        modal.classList.replace('modal', 'modal-visible')
+    }
+})
+
+closemodal.addEventListener('click', ()=>{
+    modal.classList.replace('modal-visible', 'modal');
+})
 
 
 botonBurger.addEventListener('click', ()=>{
     
     if((asideLeft.classList)=='aside-left'){
-        console.log("tiene la clase aside-left");
         asideLeft.classList.replace('aside-left', 'remove-aside-left')
         containerMain.classList.replace('container-page-main', 'add-container-page-main')
         pagemain.classList.replace('container-main','add-container-main')
         botonBurger.style.transform="rotate(90deg)"
     }
     else{
-        console.log("no tinene la clase");
         asideLeft.classList.replace('remove-aside-left', 'aside-left' )
         containerMain.classList.replace('add-container-page-main', 'container-page-main')
         pagemain.classList.replace('add-container-main', 'container-main')
@@ -25,10 +39,3 @@ botonBurger.addEventListener('click', ()=>{
 });
 
 
-/* botonBurger.addEventListener('click', ()=>{
-
-    console.log(asideLeft.contains('aside-left'));
-    if(asideLeft.contains('aside-left')==true){
-
-    }
-}) */
